189bf446a8cfe05aeab974057317624dbbf51bc5
ktoan911 <khanhtoan.forwork@gmail.com>
2025-04-18 00:51:13 +0700

    condition

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const homepageRoutes = require("./routes/homepageRoutes");
 const memberRoutes = require("./routes/memberRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const packageRoutes = require("./routes/packageRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 8001;
@@ -23,8 +24,9 @@ app.use(express.static('public'));
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminUserRoutes);
 app.use("/api", homepageRoutes);
-app.use("/api", homepageRoutes);
 app.use("/api/members", memberRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/packages", packageRoutes);
+app.use("/api/user", userRoutes);
+
 app.listen(PORT, () => console.log(`Server run in port: ${PORT}`));
