bc70e4b3464ab77596a65c5ad31822eeab8777e7
nhatquangpx <doannhatquang0@gmail.com>
2025-06-05 16:38:38 +0700

    Add register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,6 +4,20 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const { sendEmail, sendNewPasswordEmail, sendReceiptEmail, sendMaintenanceNotificationEmail } = require("../utils/emailService");
 
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password, phone, role } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email đã tồn tại!" });
+    }
+    const newUser = new User({ name, email, password, phone, role });
+    await newUser.save();
+    res.status(200).json({ message: "Đăng ký thành công!", newUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 exports.checkExistedEmail = async (req, res) => {
   try {
     const { email } = req.body;
